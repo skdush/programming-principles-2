3cd335279a36bd937f041fa70603989da03d9843
Kaldygulov Alikhan <alihankaldygulov25gmail.com@MacBook-Pro-Kaldygulov.local>
2026-02-04 00:14:31 +0500

    Python Boolean

---
+++ practice2/Python_Operators/7.py
@@ -0,0 +1,44 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+ 
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
